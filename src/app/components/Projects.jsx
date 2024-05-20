0b62128efa2c5f9ff512e8a2f823de7251e86ff2
MinhaalAzhar94 <m.usmannoor90@gmail.com>
2024-05-20 13:33:42 +0500

    "Updated resume PDF and Projects.jsx component with new content, formatting, and technologies"

--- src/app/components/Projects.jsx
+++ src/app/components/Projects.jsx
@@ -16,7 +16,7 @@ import {
 } from "react-icons/fa6";
 import Image from "next/image";
 import { IoLogoJavascript } from "react-icons/io5";
-import { TbBrandNextjs } from "react-icons/tb";
+import { TbBrandFramerMotion, TbBrandNextjs } from "react-icons/tb";
 import { SiMysql, SiTailwindcss, SiTypescript, SiNext } from "react-icons/si";
 import { DiMsqlServer } from "react-icons/di";
 import { AiFillApi } from "react-icons/ai";
@@ -304,7 +304,10 @@ function Projects() {
               <div
                 className="flex gap-1 items-center mb-2 cursor-pointer  "
                 onClick={() =>
-                  window.open("https://musmannoor.vercel.app", "_blank")
+                  window.open(
+                    "https://musmannoorv1updated.vercel.app/",
+                    "_blank"
+                  )
                 }
               >
                 <h2 className="lg:text-white font-bold text-[#5CBE2E] group-hover:text-[#5CBE2E] sm:text-xl text-[14px] whitespace-nowrap  transition-[all_0.1s] ease-linear ">
@@ -339,6 +342,64 @@ function Projects() {
             </span>
             <span className=" inline-flex gap-[1px] items-center text-xs uppercase font-[500] text-[#5CBE2E]  bg-[#5CBE2E1f] px-3 py-[6px] rounded-[18px] ">
               NextJs <TbBrandNextjs />
+            </span>
+          </div>
+        </div>
+        <div className="  rounded-[6px] lg:group-hover/list:opacity-50 hover:!opacity-100 w-full border-[1px] border-transparent  hover:border-[1px] hover:border-[rgba(0,0,0,.2)] hover:bg-[rgba(0,0,0,0.1)]  hover:![box-shadow:0_0px_10px_0_rgba(92,190,46,0.2)] hover:backdrop-filter hover:backdrop-blur-[5px] transition-[all_0.1s] ease-linear sm:px-6 sm:py-6 p-2  group lg:bg-transparent bg-[rgba(0,0,0,0)] lg:!shadow-none ![box-shadow:0_0px_10px_0_rgba(92,190,46,0.2)] lg:backdrop-filter-none   backdrop-filter backdrop-blur-[5px]  ">
+          <div className="flex gap-5 sm:flex-nowrap  flex-wrap  ">
+            <Image
+              src={Portfolio}
+              alt=""
+              className="w-[100px] h-[80px]   rounded-md border-[1px] border-solid border-[#cac0b33d] group-hover:border-[#cac0b3]   transition-[all_0.1s] ease-linear "
+            />
+            <div>
+              <div
+                className="flex gap-1 items-center mb-2 cursor-pointer  "
+                onClick={() =>
+                  window.open(
+                    "https://musmannoorv2updated.vercel.app/",
+                    "_blank"
+                  )
+                }
+              >
+                <h2 className="lg:text-white font-bold text-[#5CBE2E] group-hover:text-[#5CBE2E] sm:text-xl text-[14px] whitespace-nowrap  transition-[all_0.1s] ease-linear ">
+                  M Usman Portfolio v2
+                </h2>
+                <MdOutlineArrowOutward className="group-hover:rotate-45 group-hover:translate-x-2  transition-[all_0.1s] ease-linear  " />
+              </div>
+              <p className=" text-[#CAC0B3]  sm:text-[15px] text-[13px] ">
+                After two years of dedicated learning and project creation, I
+                present Version 1 of my portfolio. Explore my curated projects
+                showcasing continuous growth and innovation. Find the
+                open-source project on my GitHub - feel free to explore, clone,
+                or integrate it into your work.
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-1 flex-wrap items-center mt-3">
+            <span className=" inline-flex gap-[1px] items-center text-xs uppercase font-[500] text-[#5CBE2E]  bg-[#5CBE2E1f] px-3 py-[6px] rounded-[18px] ">
+              html <FaHtml5 />
+            </span>
+            <span className=" inline-flex gap-[1px] items-center text-xs uppercase font-[500] text-[#5CBE2E]  bg-[#5CBE2E1f] px-3 py-[6px] rounded-[18px] ">
+              css <FaCss3Alt />
+            </span>
+            <span className="inline-flex gap-[2px] items-center text-xs uppercase font-[500] text-[#5CBE2E]  bg-[#5CBE2E1f] px-3 py-[6px] rounded-[18px] ">
+              tailwind <SiTailwindcss />
+            </span>
+            <span className=" inline-flex gap-[1px] items-center text-xs uppercase font-[500] text-[#5CBE2E]  bg-[#5CBE2E1f] px-3 py-[6px] rounded-[18px] ">
+              Javascript <IoLogoJavascript />
+            </span>
+            <span className=" inline-flex gap-[1px] items-center text-xs uppercase font-[500] text-[#5CBE2E]  bg-[#5CBE2E1f] px-3 py-[6px] rounded-[18px] ">
+              ReactJs <FaReact />
+            </span>
+            <span className=" inline-flex gap-[1px] items-center text-xs uppercase font-[500] text-[#5CBE2E]  bg-[#5CBE2E1f] px-3 py-[6px] rounded-[18px] ">
+              NextJs <TbBrandNextjs />
+            </span>
+            <span className=" inline-flex gap-[1px] items-center text-xs uppercase font-[500] text-[#5CBE2E]  bg-[#5CBE2E1f] px-3 py-[6px] rounded-[18px] ">
+              Framer Motion <TbBrandFramerMotion />
+            </span>
+            <span className=" inline-flex gap-[1px] items-center text-xs uppercase font-[500] text-[#5CBE2E]  bg-[#5CBE2E1f] px-3 py-[6px] rounded-[18px] ">
+              GSAP
             </span>
           </div>
         </div>
