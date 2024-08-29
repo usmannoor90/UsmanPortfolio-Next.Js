e1d33c1ab9b8aca14e8550ead0d02bb05eb84b8b
MinhaalAzhar94 <m.usmannoor90@gmail.com>
2024-08-29 17:41:34 +0500

    "Updated About component with rewritten text and added new paragraphs, removed one paragraph and commented out another"

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -5,11 +5,20 @@ function About() {
   return (
     <div className="lg:pt-16">
       <h2 className="CustomPara1 sm:text-[16px] text-[14px] font-normal   ">
-        Back in 2021, I made the switch from nuts and bolts as a freshly
-        graduated Mechanical Engineer to weaving digital wonders in the realm of
-        web development.
+        Seasoned Full Stack Software Engineer with 3+ years of experience
+        specializing in React.js, Next.js, Node.js, PostgreSQL, MongoDB, and AWS
+        serverless architectures. Proven expertise in transforming creative
+        concepts into practical, high-performance solutions. Excels in front-end
+        development using React.js/Next.js and backend engineering with
+        Node.js/Express.js.
       </h2>
-      <h2 className="CustomPara1 sm:text-[16px] text-[14px]  mt-5">
+      <h2 className="CustomPara1 sm:text-[16px] text-[14px] font-normal   ">
+        Proficient in AWS, Docker, Linux, Python, and Bash scripting, with a
+        strong focus on enhancing DevOps capabilities. Committed to staying
+        informed about industry advancements and continuously honing skills to
+        consistently deliver exceptional results.
+      </h2>
+      {/* <h2 className="CustomPara1 sm:text-[16px] text-[14px]  mt-5">
         My inaugural year with{" "}
         <button className="text-white  font-semibold capitalize barDown hover:text-[#5CBE2E] cursor-none ">
           React
@@ -31,7 +40,7 @@ function About() {
           DevOps
         </button>{" "}
         capabilities.
-      </h2>
+      </h2> */}
       <h2 className="CustomPara1 sm:text-[16px] text-[14px]  mt-5">
         Beyond the keyboard, I’m the{" "}
         <span className="text-white  font-semibold CustomCarpanterCursor capitalize pb-[3px]  barDown hover:text-[#5CBE2E] ">
